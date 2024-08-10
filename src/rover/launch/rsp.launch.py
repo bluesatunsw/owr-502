c6f1ba0188ecc5b89a2ea01a8ad73e2533b64895
Aavin Fernando <aavin.fernando@gmail.com>
2024-08-10 14:55:36 +0000

    [Drivebase] Adds URDF for rover, sanitize xacro input

--- src/rover/launch/rsp.launch.py
+++ src/rover/launch/rsp.launch.py
@@ -2,6 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+from launch_ros.descriptions import ParameterValue
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
@@ -19,11 +20,10 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('rover'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    # robot_description_config = xacro.process_file(xacro_file).toxml()
     robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    params = {'robot_description': ParameterValue(robot_description_config, value_type=str), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
