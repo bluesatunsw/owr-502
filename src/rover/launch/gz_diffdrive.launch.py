899b8a81253c15dec9daf3b645f98a972ef949ea
Aavin Fernando <aavin.fernando@gmail.com>
2025-03-14 04:44:22 +0000

    Reformat rover w/ python-black

--- src/rover/launch/gz_diffdrive.launch.py
+++ src/rover/launch/gz_diffdrive.launch.py
@@ -17,7 +17,12 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
 
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
@@ -26,82 +31,102 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # Launch Arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    use_sim_time = LaunchConfiguration("use_sim_time", default=True)
 
     # Get URDF via xacro
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name='xacro')]), ' ',
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
             PathJoinSubstitution(
-                [FindPackageShare('rover'),
-                 'description', 'robot.urdf.xacro']
+                [FindPackageShare("rover"), "description", "robot.urdf.xacro"]
             ),
         ]
     )
-    robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
     robot_controllers = PathJoinSubstitution(
         [
-            FindPackageShare('rover'),
-            'config', "drivebase",
-            'diff_drive_controller.yaml',
+            FindPackageShare("rover"),
+            "config",
+            "drivebase",
+            "diff_drive_controller.yaml",
         ]
     )
 
     node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
     )
 
     gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-topic', 'robot_description', '-name',
-                   'rover', '-allow_renaming', 'true'],
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-topic",
+            "robot_description",
+            "-name",
+            "rover",
+            "-allow_renaming",
+            "true",
+        ],
     )
 
     joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
     )
     diff_drive_base_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
+        package="controller_manager",
+        executable="spawner",
         arguments=[
-            'diff_drive_base_controller',
-            '--param-file',
+            "diff_drive_base_controller",
+            "--param-file",
             robot_controllers,
-            ],
+        ],
     )
 
-    return LaunchDescription([
-        # Launch gazebo environment
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-                                       'launch',
-                                       'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [' -r -v 4 dem_moon.sdf'])]),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[joint_state_broadcaster_spawner],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[diff_drive_base_controller_spawner],
-            )
-        ),
-        node_robot_state_publisher,
-        gz_spawn_entity,
-        # Launch Arguments
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value=use_sim_time,
-            description='If true, use simulated clock'),
-    ])
+    return LaunchDescription(
+        [
+            # Launch gazebo environment
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        PathJoinSubstitution(
+                            [
+                                FindPackageShare("ros_gz_sim"),
+                                "launch",
+                                "gz_sim.launch.py",
+                            ]
+                        )
+                    ]
+                ),
+                launch_arguments=[("gz_args", [" -r -v 4 dem_moon.sdf"])],
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=gz_spawn_entity,
+                    on_exit=[joint_state_broadcaster_spawner],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=joint_state_broadcaster_spawner,
+                    on_exit=[diff_drive_base_controller_spawner],
+                )
+            ),
+            node_robot_state_publisher,
+            gz_spawn_entity,
+            # Launch Arguments
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value=use_sim_time,
+                description="If true, use simulated clock",
+            ),
+        ]
+    )
--- src/rover/launch/gz_swervedrive.launch.py
+++ src/rover/launch/gz_swervedrive.launch.py
@@ -17,7 +17,12 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
 
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
@@ -25,90 +30,99 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Launch Arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-
     # Get URDF via xacro
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name='xacro')]), ' ',
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
             PathJoinSubstitution(
-                [FindPackageShare('rover'),
-                 'description', 'robot.urdf.xacro']
+                [FindPackageShare("rover"), "description", "robot.urdf.xacro"]
             ),
         ]
     )
-    robot_description = {'robot_description': ParameterValue(robot_description_content, value_type=str)}
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
     robot_controllers = PathJoinSubstitution(
         [
-            FindPackageShare('rover'),
-            'config', "drivebase",
-            'swerve_drive_controller.yaml',
+            FindPackageShare("rover"),
+            "config",
+            "drivebase",
+            "swerve_drive_controller.yaml",
         ]
     )
 
     node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
     )
 
     gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
         arguments=[
-            '-topic', 'robot_description', 
-            '-name', 'rover', 
-            '-allow_renaming', 'true',
-            '-z', '7.6'                     # Spawn ABOVE the surface
+            "-topic",
+            "robot_description",
+            "-name",
+            "rover",
+            "-allow_renaming",
+            "true",
+            "-z",
+            "7.6",  # Spawn ABOVE the surface
         ],
     )
 
     joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
     )
     swerve_drive_base_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
+        package="controller_manager",
+        executable="spawner",
         arguments=[
-            'swerve_drive_base_controller',
-            '--param-file',
+            "swerve_drive_base_controller",
+            "--param-file",
             robot_controllers,
-            ],
+        ],
     )
 
-    return LaunchDescription([
-        # Launch gazebo environment
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-                                       'launch',
-                                       'gz_sim.launch.py'])]),
-            launch_arguments={
-                'gz_args' : [' -r -v 4 dem_moon.sdf'],
-                'on_exit_shutdown' : 'true'
-            }.items()),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[joint_state_broadcaster_spawner],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster_spawner,
-                on_exit=[swerve_drive_base_controller_spawner],
-            )
-        ),
-        node_robot_state_publisher,
-        gz_spawn_entity,
-        # Launch Arguments
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value=use_sim_time,
-            description='If true, use simulated clock'),
-    ])
+    return LaunchDescription(
+        [
+            # Launch gazebo environment
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        PathJoinSubstitution(
+                            [
+                                FindPackageShare("ros_gz_sim"),
+                                "launch",
+                                "gz_sim.launch.py",
+                            ]
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    "gz_args": [" -r -v 4 dem_moon.sdf"],
+                    "on_exit_shutdown": "true",
+                }.items(),
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=gz_spawn_entity,
+                    on_exit=[joint_state_broadcaster_spawner],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=joint_state_broadcaster_spawner,
+                    on_exit=[swerve_drive_base_controller_spawner],
+                )
+            ),
+            node_robot_state_publisher,
+            gz_spawn_entity,
+        ]
+    )
