f0b147b5b4e411cdce94e9ce20d77b7ace71ba28
Jonah Hopkin <jonahhopkin@gmail.com>
2025-03-21 18:07:05 +1100

    Add morse code for debugging, further attempts to init CAN

--- src/stepper_controller/stepper_controller.ino
+++ src/stepper_controller/stepper_controller.ino
@@ -12,6 +12,7 @@
 
 extern "C" void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan);
 void blinkLED(int times, int blinkDur);
+void blinkMorse(char *s);
 
 // stand-ins for the actual CAN IDs of the stepper motors
 enum StepperId {
@@ -19,7 +20,7 @@ enum StepperId {
 };
 
 HAL_StatusTypeDef halStatus = {};
-CAN_HandleTypeDef hcan = {};
+CAN_HandleTypeDef hcan_ = {};
 
 ///////////////////////////
 // CAN-related functions //
@@ -31,41 +32,69 @@ struct CANFrame {
   uint32_t data {};
 };
 
-void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan) {
+void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan_) {
   __HAL_RCC_CAN1_CLK_ENABLE();
 }
 
 void initialiseCAN() {
-  // TODO: load up hcan with a bunch of config parameters
-  halStatus = HAL_CAN_Init(&hcan);
+  hcan_.Instance = CAN;
+  /* TODO: comb through CAN reference spec and check that these values all sensible for what we're doing */
+  CAN_InitTypeDef *init = &(hcan_.Init);
+  init->Prescaler = 1; /* "length of a time quantum", between 1 and 1024 */
+  init->Mode = CAN_MODE_NORMAL;
+  init->SyncJumpWidth = CAN_SJW_1TQ;
+  init->TimeSeg1 = CAN_BS1_1TQ;
+  init->TimeSeg2 = CAN_BS2_1TQ;
+  init->TimeTriggeredMode = ENABLE;
+  init->AutoBusOff = ENABLE;
+  init->AutoWakeUp = ENABLE;
+  init->AutoRetransmission = DISABLE;
+  init->ReceiveFifoLocked = DISABLE;
+  init->TransmitFifoPriority = DISABLE;
+
+  halStatus = HAL_CAN_Init(&hcan_);
+
   // bedugging!
   if (halStatus == HAL_OK) {
-    blinkLED(3, FAST_BLINK_TIME_MS);
+    blinkMorse("ok");
   } else if (halStatus == HAL_ERROR) {
-    blinkLED(1, FAST_BLINK_TIME_MS);
-    blinkLED(2, SLOW_BLINK_TIME_MS);
-    blinkLED(1, FAST_BLINK_TIME_MS);
+    blinkMorse("error");
   } else if (halStatus == HAL_BUSY) {
-    blinkLED(1, SLOW_BLINK_TIME_MS);
-    blinkLED(2, FAST_BLINK_TIME_MS);
-    blinkLED(1, SLOW_BLINK_TIME_MS);
+    blinkMorse("busy");
   } else if (halStatus == HAL_TIMEOUT) {
-    blinkLED(6, FAST_BLINK_TIME_MS);
+    blinkMorse("timeout");
   } else {
-    blinkLED(4, BLINK_TIME_MS);
+    blinkMorse("unknown");
   }
 
-  // HAL_CAN_ConfigFilter()?
-  // hal_status = HAL_CAN_Start(hcan);
+  // TODO: HAL_CAN_ConfigFilter()?
+  
+  halStatus = HAL_CAN_Start(&hcan_); // this currently errors, possibly we need to config filter
+  
+  if (halStatus == HAL_OK) {
+    blinkMorse("ok");
+  } else if (halStatus == HAL_ERROR) {
+    blinkMorse("error");
+  } else if (halStatus == HAL_BUSY) {
+    blinkMorse("busy");
+  } else if (halStatus == HAL_TIMEOUT) {
+    blinkMorse("timeout");
+  } else {
+    blinkMorse("unknown");
+  }
 }
 
 // blocks until a CAN frame becomes available
 CANFrame getFrame() {
-  CANFrame frame;
-  // generate a dummy frame
-  // TODO: actually read from CAN!
-  frame.id = 32;
-  frame.data = 5;
+  // do we need to monitor both receive FIFOs? does the hardware automatically fill both or just one?
+  while (HAL_CAN_GetRxFifoFillLevel(&hcan_, CAN_RX_FIFO0) == 0) {
+    blinkLED(1, SLOW_BLINK_TIME_MS);
+  };
+  CAN_RxHeaderTypeDef frameHeader;
+  uint8_t framePayload[8];
+  halStatus = HAL_CAN_GetRxMessage(&hcan_, CAN_RX_FIFO0, &frameHeader, framePayload);
+  // TODO: check this works and copy the data into the CANFrame struct
+  CANFrame frame = {};
   return frame;
 }
 
@@ -100,18 +129,72 @@ void blinkSOS() {
   delay(800);
 }
 
+// printf like it's 1844
+void blinkMorse(char *s) {
+  char c = s[0];
+  const char *morse_lookup[] = {
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  };
+  for (int i = 0;; i++) {
+    c = s[i];
+    if (c == '\0') break;
+    if (c >= 'A' && c <= 'Z') {
+      c = c - 'A';
+    } else if (c >= 'a' && c <= 'z') {
+      c = c - 'a';
+    } else  {
+      // invalid character
+      c = 'x' - 'a';
+    }
+    const char *pattern = morse_lookup[c];
+    while (*pattern != '\0') {
+      if (*pattern == '-') {
+        blinkLED(1, SLOW_BLINK_TIME_MS);
+      } else {
+        blinkLED(1, FAST_BLINK_TIME_MS);
+      }
+      pattern++;
+    }
+    delay(500);
+  }
+}
+
 ///////////////////////
 // Arduino functions //
 ///////////////////////
 
 void setup() {
   initialiseBlinker();
-  delay(1000);
   initialiseCAN();
 }
 
 void loop() {
-  //blinkSOS();
+  // blinkSOS();
   CANFrame frame = getFrame();
   if (frame.id >= STEPPER_A && frame.id <= STEPPER_C) {
     blinkLED(frame.data, FAST_BLINK_TIME_MS);
