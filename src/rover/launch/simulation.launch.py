27461fd067e560e5b9394cd12f5a47882a865bc4
Alys <alys.weiss@student.unsw.edu.au>
2025-03-21 03:11:58 +0000

    Testing camera real-sense

--- src/rover/launch/simulation.launch.py
+++ src/rover/launch/simulation.launch.py
@@ -1,5 +1,16 @@
-import launch
-import launch_ros.actions
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
@@ -74,17 +85,29 @@ def generate_launch_description():
         ],
     )
 
+    bridge_params = PathJoinSubstitution(
+        [
+            FindPackageShare("rover"),
+            "config",
+            "gazebo",
+            "bridge.yaml",
+        ]
+    )
+
+    start_gazebo_ros_bridge_cmd = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[{"config_file": bridge_params}],
+        output="screen",
+    )
+
     return LaunchDescription(
         [
+            # Launch gazebo environment
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     [
                         PathJoinSubstitution(
-                            [
-                                FindPackageShare("realsense_camera"),
-                                "launch",
-                                "rs_launch.py",
-                            ], 
                             [
                                 FindPackageShare("ros_gz_sim"),
                                 "launch",
@@ -96,9 +119,6 @@ def generate_launch_description():
                 launch_arguments={
                     "gz_args": [" -r -v 4 dem_moon.sdf"],
                     "on_exit_shutdown": "true",
-                    "enable_color": "true",
-                    "enable_depth": "true",
-                    "pointcloud.enable": "true"
                 }.items(),
             ),
             RegisterEventHandler(
@@ -112,6 +132,3 @@ def generate_launch_description():
             start_gazebo_ros_bridge_cmd,
         ]
     )
-
-
-
