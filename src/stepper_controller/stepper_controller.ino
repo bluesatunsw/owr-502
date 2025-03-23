6281488595fd896c6835f6113ebf42703cd0aa2a
Jonah Hopkin <jonahhopkin@gmail.com>
2025-03-23 23:42:05 +1100

    CAN now starts with status OK

--- src/stepper_controller/stepper_controller.ino
+++ src/stepper_controller/stepper_controller.ino
@@ -9,6 +9,10 @@
 #define BLINK_TIME_MS       300
 #define FAST_BLINK_TIME_MS  150
 #define SLOW_BLINK_TIME_MS  600
+#define BITRATE             1000000
+
+#define VERY_FAST_BLINK_TIME_MS  50
+#define VERY_SLOW_BLINK_TIME_MS  2000
 
 extern "C" void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan);
 void blinkLED(int times, int blinkDur);
@@ -27,74 +31,193 @@ CAN_HandleTypeDef hcan_ = {};
 ///////////////////////////
 
 struct CANFrame {
-  // TODO: add fields
-  uint32_t id {};
-  uint32_t data {};
+  uint32_t id;
+  CAN_RxHeaderTypeDef header;
+  uint8_t data[8];
+};
+
+struct CanTiming {
+  uint32_t prescaler;
+  uint32_t sjw;
+  uint32_t tseg1;
+  uint32_t tseg2;
 };
 
 void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan_) {
   __HAL_RCC_CAN1_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /* initialise CAN pins (PB8 = CAN_RX, PB9 = CAN_TX) */
+  GPIO_InitTypeDef CANRx = {
+    .Pin = GPIO_PIN_8,
+    .Mode = GPIO_MODE_AF_OD,
+    .Pull = GPIO_NOPULL,
+    /* TODO: set speed dynamically based on bitrate/solved clock division? */
+    //.Speed = GPIO_SPEED_FREQ_MEDIUM, /* 4 to 10 MHz? */
+    .Speed = GPIO_SPEED_FREQ_LOW, /* up to 4 MHz? */
+    .Alternate = GPIO_AF4_CAN,
+  };
+  GPIO_InitTypeDef CANTx = {
+    .Pin = GPIO_PIN_9,
+    .Mode = GPIO_MODE_AF_OD,
+    .Pull = GPIO_NOPULL,
+    //.Speed = GPIO_SPEED_FREQ_MEDIUM, /* 4 to 10 MHz? */
+    .Speed = GPIO_SPEED_FREQ_LOW, /* up to 4 MHz? */
+    .Alternate = GPIO_AF4_CAN,
+  };
+  HAL_GPIO_Init(GPIOB, &CANRx);
+  HAL_GPIO_Init(GPIOB, &CANTx);
+}
+
+/* taken from Aavin's SimpleCAN's BaseCAN.cpp */
+CanTiming solveCanTiming(uint32_t clockFreq, uint32_t bitrate,
+                         uint8_t multiplier) {
+  // cf. http://www.bittiming.can-wiki.info/
+  CanTiming timing = {};
+  uint32_t baseQuanta = 16;
+  uint32_t timeQuanta = baseQuanta;
+
+  uint32_t offset = 0;
+  bool found = false;
+
+  // start at 16 and work outwards
+  while (offset <= 9) {
+    // Looking for a timeQuanta of between 8 and 25.
+    timeQuanta = baseQuanta - offset;
+    if (clockFreq % (bitrate * timeQuanta * multiplier) == 0) {
+      found = true;
+      break;
+    }
+    timeQuanta = baseQuanta + offset;
+    if (clockFreq % (bitrate * timeQuanta * multiplier) == 0) {
+      found = true;
+      break;
+    }
+    offset += 1;
+  }
+
+  if (!found) {
+    blinkMorse("nosolve");
+  }
+
+  timing.prescaler = clockFreq / (bitrate * timeQuanta);
+  timing.sjw = 1;
+  timing.tseg1 = uint32_t(0.875 * timeQuanta) - 1;
+
+  float samplePoint = (1.0 + timing.tseg1) / timeQuanta;
+  float samplePoint2 = (1.0 + timing.tseg1 + 1) / timeQuanta;
+
+  if (abs(samplePoint2 - 0.875) < abs(samplePoint - 0.875)) {
+    timing.tseg1 += 1;
+    samplePoint = samplePoint2;
+  }
+
+  timing.tseg2 = timeQuanta - timing.tseg1 - 1;
+#ifdef CAN_DEBUG
+  _Serial->print("clockFreq:");
+  _Serial->print(clockFreq);
+  _Serial->print(", bitrate:");
+  _Serial->print(bitrate);
+  _Serial->print(", prescaler:");
+  _Serial->print(timing.prescaler);
+  _Serial->print(", timeQuanta:");
+  _Serial->print(timeQuanta);
+  _Serial->print(", nominalTimeSeg1:");
+  _Serial->print(timing.tseg1);
+  _Serial->print(", nominalTimeSeg2:");
+  _Serial->print(timing.tseg2);
+  _Serial->print(", samplePoint:");
+  _Serial->println(samplePoint);
+
+#endif
+  return timing;
+}
+
+void blinkHalStatus(HAL_StatusTypeDef status) {
+  if (status == HAL_OK) {
+    blinkMorse("ok");
+  } else if (status == HAL_ERROR) {
+    if (hcan_.ErrorCode == HAL_CAN_ERROR_TIMEOUT) {
+      blinkMorse("errtime");
+    }
+    blinkMorse("error");
+  } else if (status == HAL_BUSY) {
+    blinkMorse("busy");
+  } else if (status == HAL_TIMEOUT) {
+    blinkMorse("timeout");
+  } else {
+    blinkMorse("unknown");
+  }
 }
 
 void initialiseCAN() {
+  /* CAN "peripheral clock" is the APB clock (PCLK) */
+  /* which is derived from SYSCLK (8 MHz) by default -- refer to clock tree */
+  uint32_t clockFreq = HAL_RCC_GetPCLK1Freq();
+  CanTiming timing = solveCanTiming(clockFreq, BITRATE, 1);
+
   hcan_.Instance = CAN;
-  /* TODO: comb through CAN reference spec and check that these values all sensible for what we're doing */
   CAN_InitTypeDef *init = &(hcan_.Init);
-  init->Prescaler = 1; /* "length of a time quantum", between 1 and 1024 */
-  init->Mode = CAN_MODE_NORMAL;
-  init->SyncJumpWidth = CAN_SJW_1TQ;
-  init->TimeSeg1 = CAN_BS1_1TQ;
-  init->TimeSeg2 = CAN_BS2_1TQ;
+  init->Prescaler = timing.prescaler;
+  //init->Mode = CAN_MODE_NORMAL;
+  init->Mode = CAN_MODE_LOOPBACK;
+  init->SyncJumpWidth = timing.sjw;
+  init->TimeSeg1 = timing.tseg1;
+  init->TimeSeg2 = timing.tseg2;
   init->TimeTriggeredMode = ENABLE;
-  init->AutoBusOff = ENABLE;
-  init->AutoWakeUp = ENABLE;
+  init->AutoBusOff = DISABLE;
+  init->AutoWakeUp = DISABLE;
   init->AutoRetransmission = DISABLE;
   init->ReceiveFifoLocked = DISABLE;
   init->TransmitFifoPriority = DISABLE;
 
   halStatus = HAL_CAN_Init(&hcan_);
 
-  // bedugging!
-  if (halStatus == HAL_OK) {
-    blinkMorse("ok");
-  } else if (halStatus == HAL_ERROR) {
-    blinkMorse("error");
-  } else if (halStatus == HAL_BUSY) {
-    blinkMorse("busy");
-  } else if (halStatus == HAL_TIMEOUT) {
-    blinkMorse("timeout");
-  } else {
-    blinkMorse("unknown");
-  }
+  /*
+   * Currently, we're just trying to pick up all CAN frames to make sure that we
+   * can actually pick them up, but for the actual driver we should configure
+   * three separate filters (w/ CAN_FILTERMODE_IDLIST) corresponding to the
+   * three stepper IDs.
+   */
+  CAN_FilterTypeDef filterConf = {
+    .FilterIdHigh = 0x0000,
+    .FilterIdLow = 0x0000,
+    .FilterMaskIdHigh = 0x0000,
+    .FilterMaskIdLow = 0x0000,
+    .FilterFIFOAssignment = CAN_FILTER_FIFO0,
+    .FilterBank = 0,
+    //.FilterMode = CAN_FILTERMODE_IDLIST,
+    .FilterMode = CAN_FILTERMODE_IDMASK,
+    .FilterScale = CAN_FILTERSCALE_32BIT,
+    .FilterActivation = CAN_FILTER_ENABLE,
+    .SlaveStartFilterBank = 0
+  };
 
-  // TODO: HAL_CAN_ConfigFilter()?
-  
-  halStatus = HAL_CAN_Start(&hcan_); // this currently errors, possibly we need to config filter
-  
-  if (halStatus == HAL_OK) {
-    blinkMorse("ok");
-  } else if (halStatus == HAL_ERROR) {
-    blinkMorse("error");
-  } else if (halStatus == HAL_BUSY) {
-    blinkMorse("busy");
-  } else if (halStatus == HAL_TIMEOUT) {
-    blinkMorse("timeout");
-  } else {
-    blinkMorse("unknown");
-  }
+  halStatus = HAL_CAN_ConfigFilter(&hcan_, &filterConf);
+
+  halStatus = HAL_CAN_Start(&hcan_);
+  blinkHalStatus(halStatus);
 }
 
-// blocks until a CAN frame becomes available
+/* blocks until a CAN frame becomes available */
+/* untested */
 CANFrame getFrame() {
-  // do we need to monitor both receive FIFOs? does the hardware automatically fill both or just one?
+  /* as per the filter(s) we've set up, we expect messages to go into FIFO0 */
+  CANFrame frame = {};
   while (HAL_CAN_GetRxFifoFillLevel(&hcan_, CAN_RX_FIFO0) == 0) {
-    blinkLED(1, SLOW_BLINK_TIME_MS);
+    blinkLED(1, VERY_SLOW_BLINK_TIME_MS);
   };
   CAN_RxHeaderTypeDef frameHeader;
   uint8_t framePayload[8];
   halStatus = HAL_CAN_GetRxMessage(&hcan_, CAN_RX_FIFO0, &frameHeader, framePayload);
-  // TODO: check this works and copy the data into the CANFrame struct
-  CANFrame frame = {};
+  blinkHalStatus(halStatus);
+  if (halStatus == HAL_ERROR) return frame;
+  blinkLED(10, VERY_FAST_BLINK_TIME_MS);
+  frame.id = STEPPER_A;
+  frame.header = frameHeader;
+  for (int i = 0; i < 8; i++) {
+    frame.data[i] = framePayload[i];
+  }
   return frame;
 }
 
@@ -197,6 +320,6 @@ void loop() {
   // blinkSOS();
   CANFrame frame = getFrame();
   if (frame.id >= STEPPER_A && frame.id <= STEPPER_C) {
-    blinkLED(frame.data, FAST_BLINK_TIME_MS);
+    blinkLED(frame.data[7], FAST_BLINK_TIME_MS);
   }
 }
