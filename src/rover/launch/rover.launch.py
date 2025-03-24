8e0f13350317e21550320a79b3a8a6e350d9c24c
Evan La Fontaine <navelafontaine@gmail.com>
2025-03-24 06:54:16 +0000

    Restor rover stuf

--- src/rover/launch/rover.launch.py
+++ src/rover/launch/rover.launch.py
@@ -1,16 +1,5 @@
-# Copyright 2022 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import launch
+import launch_ros.actions
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
@@ -50,13 +39,6 @@ def generate_launch_description():
             "drivebase",
             "swerve_drive_controller.yaml",
         ]
-    )
-
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[],
-        output="both",
     )
 
     node_robot_state_publisher = Node(
@@ -106,3 +88,6 @@ def generate_launch_description():
             node_robot_state_publisher,
         ]
     )
+
+
+
