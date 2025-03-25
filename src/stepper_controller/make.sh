45f386d7d90ee24c53da4f8a635f0c71bf475e31
Jonah Hopkin <jonahhopkin@gmail.com>
2025-03-25 16:42:37 +1100

    Add fast math and optimisation to build options

--- src/stepper_controller/make.sh
+++ src/stepper_controller/make.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-arduino-cli compile -b STMicroelectronics:stm32:GenF0 -e --board-options "pnum=GENERIC_F072RBTX"
+arduino-cli compile -b STMicroelectronics:stm32:GenF0 -e --board-options "pnum=GENERIC_F072RBTX" --build-property "build.extra_flags=\"-ffast-math\"" --build-property "build.extra_flags=\"-O3\""
