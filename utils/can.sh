ee2cbe7729f173e5eb042bf2e346c63788ea2a0a
Wark Maldron <owr@warkm.maldron>
2025-03-29 16:53:21 +1100

    Can and WASD bash scripts

--- utils/can.sh
+++ utils/can.sh
@@ -7,5 +7,5 @@ sudo modprobe can_raw
 sudo modprobe mttcan
 
 sudo ip link set can0 down 
-sudo ip link set can0 type can bitrate 1000000 fd off restart-ms 10 berr-reporting on loopback off
+sudo ip link set can0 type can bitrate 1000000 fd off restart-ms 10 berr-reporting off loopback off
 sudo ip link set can0 up
