fc6d3da80128fdc3faa4f4f432ef11c72016be33
Wark Maldron <osr@owr-wark.NUC>
2025-07-08 11:03:04 +0000

    Setup repo for mini-pc

--- utils/can.sh
+++ utils/can.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo rmmod mttcan
+#sudo rmmod mttcan
 
 sudo modprobe can
 sudo modprobe can_raw
-sudo modprobe mttcan
+#sudo modprobe mttcan
 
 sudo ip link set can0 down 
 sudo ip link set can0 type can bitrate 1000000 fd off restart-ms 10 berr-reporting on loopback off
