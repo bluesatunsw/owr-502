f7a50e51cf55e52f287c03993d58cadf4e7c2fbf
Jonah Hopkin <jonahhopkin@gmail.com>
2025-09-29 12:50:29 +1000

    Upgrade to canadensis v0.5.0
    Also updates bxcan to v0.8.0 and stm32f1xx-hal to v0.11.0.
    
    Most changes aren't too crazy, e.g.
    - the clock is now mandated to be a 32-bit microsecond counter
    - message publish tokens have been removed
    - setting up the bxcan peripheral is slightly cleaner
    
    However, subscriptions now use up 32KiB each! (Consequence of canadensis
    PR #53.) This means that this code will not run on a dev board with
    limited memory: it will Hard Fault. A temporary workaround is to set
    NUM_NODE_IDS or NUM_TRANSFER_IDS in canadensis_can/src/rx/subscription.rs
    to suitably small values and then avoid exceeding these limits in the
    system (e.g. by using any node-IDs greater than NUM_NODE_IDS). However,
    the new canadensis_can footprint is unacceptable, so we should consider
    rewriting the subscription system to be leaner and make a PR.

--- src/canadensis_mvp/src/boards/stm32f103/mod.rs
+++ src/canadensis_mvp/src/boards/stm32f103/mod.rs
@@ -1,4 +1,4 @@
-//! See README
+//! See boards README
 
 use canadensis::core::time as cyphal_time;
 use cortex_m::interrupt::Mutex;
@@ -6,7 +6,6 @@ use core::cell::RefCell;
 
 use stm32f1xx_hal::{
     pac,
-    prelude::*,
     rcc::{Enable, Reset},
 };
 
@@ -15,24 +14,20 @@ struct STM32F103CyphalClock {
     hw_timer_lower: pac::TIM2,
     // TIM3 allocated to the microsecond Cyphal timer, upper 16 bits
     hw_timer_upper: pac::TIM3,
-    // TODO: Add either interrupt handling (as per previous iteration of this program) or chain a
-    // third timer to extend this timer to 48 bits.
+    // TODO: Make sure 32-bit hourly overflow handled sensibly.
 }
 
 // Internal 1 MHz timer to provide time instants for Cyphal.
 impl STM32F103CyphalClock {
-    fn new(tim2: pac::TIM2, tim3: pac::TIM3) -> Self {
-        // Enable and reset the timer peripheral.
-        unsafe {
-            let rcc_ptr = &(*pac::RCC::ptr());
-            pac::TIM2::enable(rcc_ptr);
-            pac::TIM3::enable(rcc_ptr);
-            pac::TIM2::reset(rcc_ptr);
-            pac::TIM3::reset(rcc_ptr);
-            Self {
-                hw_timer_lower: tim2,
-                hw_timer_upper: tim3,
-            }
+    fn new(tim2: pac::TIM2, tim3: pac::TIM3, rcc: &mut pac::RCC) -> Self {
+        // Reset and enable the timer peripheral.
+        pac::TIM2::reset(rcc);
+        pac::TIM3::reset(rcc);
+        pac::TIM2::enable(rcc);
+        pac::TIM3::enable(rcc);
+        Self {
+            hw_timer_lower: tim2,
+            hw_timer_upper: tim3,
         }
     }
 
@@ -40,28 +35,28 @@ impl STM32F103CyphalClock {
     fn start(&mut self) {
         // updates on overflow happen automatically
         // set frequency to external clock (8MHz)
-        self.hw_timer_lower.smcr.write(|w| w.sms().bits(0b000)); // select CK_INT
-        self.hw_timer_lower.psc.write(|w| w.psc().bits(0x0007)); // scale 8MHz to 1MHz
-
-        // See STM RM0008 Section 15.3.15 Timer synchronization,
-        // "Using one timer as prescaler for another timer" for hints on this section.
-        // Configure lower timer to generate external trigger output on update (i.e. overflow).
-        self.hw_timer_lower.cr2.write(|w| w.mms().bits(0b010));
-        // Configure upper timer to be in external clock mode, clocked by ITR1
-        // => TIM3 clocked by TIM2 updates (Table 86)
         unsafe {
+            self.hw_timer_lower.smcr().write(|w| w.sms().bits(0b000)); // select CK_INT
+            self.hw_timer_lower.psc().write(|w| w.psc().bits(0x0007)); // scale 8MHz to 1MHz
+
+            // See STM RM0008 Section 15.3.15 Timer synchronization,
+            // "Using one timer as prescaler for another timer" for hints on this section.
+            // Configure lower timer to generate external trigger output on update (i.e. overflow).
+            self.hw_timer_lower.cr2().write(|w| w.mms().bits(0b010));
+            // Configure upper timer to be in external clock mode, clocked by ITR1
+            // => TIM3 clocked by TIM2 updates (Table 86)
             self.hw_timer_upper
-                .smcr
+                .smcr()
                 .write(|w| w.ts().bits(0b001).sms().bits(0b111));
+            // set counters to 0 initially
+            self.hw_timer_lower.cnt().write(|w| w.cnt().bits(0));
+            self.hw_timer_upper.cnt().write(|w| w.cnt().bits(0));
         }
-        // set counters to 0 initially
-        self.hw_timer_lower.cnt.write(|w| w.cnt().bits(0));
-        self.hw_timer_upper.cnt.write(|w| w.cnt().bits(0));
         // start timers
         self.hw_timer_lower
-            .cr1
+            .cr1()
             .write(|w| w.urs().set_bit().cen().set_bit());
-        self.hw_timer_upper.cr1.write(|w| w.cen().set_bit());
+        self.hw_timer_upper.cr1().write(|w| w.cen().set_bit());
         // desired defaults:
         // DIR = 0 -> count up
         // CMS = 00 -> edge-aligned mode
@@ -78,10 +73,10 @@ pub struct CyphalClock {}
 // exists to be instantiable locally but passes everything through to global state
 // (state has to be global to be modifiable by interrupt handler)
 impl CyphalClock {
-    pub fn new_singleton(tim2: pac::TIM2, tim3: pac::TIM3) -> Self {
+    pub fn new_singleton(tim2: pac::TIM2, tim3: pac::TIM3, rcc: &mut pac::RCC) -> Self {
         // takes in a few hardware peripherals
         cortex_m::interrupt::free(|cs| {
-            *G_CYPHAL_CLOCK.borrow(cs).borrow_mut() = Some(STM32F103CyphalClock::new(tim2, tim3))
+            *G_CYPHAL_CLOCK.borrow(cs).borrow_mut() = Some(STM32F103CyphalClock::new(tim2, tim3, rcc))
         });
         CyphalClock {}
     }
@@ -99,14 +94,14 @@ impl CyphalClock {
 }
 
 // panics if CyphalClock not initialised and started
-fn get_instant() -> cyphal_time::Microseconds48 {
+fn get_instant() -> cyphal_time::Microseconds32 {
     // TODO: Handle edge case where timer overflows in between reading upper and lower bits
     cortex_m::interrupt::free(|cs| {
         if let Some(cyphal_clock) = G_CYPHAL_CLOCK.borrow(cs).borrow_mut().as_mut() {
-            let lower_time = cyphal_clock.hw_timer_lower.cnt.read().bits();
-            cyphal_time::Microseconds48::new(cyphal_time::u48::U48::from(
-                (cyphal_clock.hw_timer_upper.cnt.read().bits() << 16) + lower_time,
-            ))
+            let lower_time = cyphal_clock.hw_timer_lower.cnt().read().bits();
+            cyphal_time::Microseconds32::from_ticks(
+                (cyphal_clock.hw_timer_upper.cnt().read().bits() << 16) + lower_time
+            )
         } else {
             panic!("Could not borrow global cyphal clock")
         }
@@ -114,9 +109,7 @@ fn get_instant() -> cyphal_time::Microseconds48 {
 }
 
 impl cyphal_time::Clock for CyphalClock {
-    type Instant = cyphal_time::Microseconds48;
-
-    fn now(&mut self) -> Self::Instant {
+    fn now(&mut self) -> cyphal_time::Microseconds32 {
         get_instant()
     }
 }
@@ -124,7 +117,7 @@ impl cyphal_time::Clock for CyphalClock {
 pub struct GeneralClock {}
 
 impl GeneralClock {
-    pub fn now() -> cyphal_time::Microseconds48 {
+    pub fn now() -> cyphal_time::Microseconds32 {
         get_instant()
     }
 }
