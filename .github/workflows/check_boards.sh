76b972cdd048ee0a6fd63344bfa1b093bdd77e2f
Aavin Fernando <aavin.fernando@gmail.com>
2026-02-28 16:09:04 +1100

    Nicer printing

--- .github/workflows/check_boards.sh
+++ .github/workflows/check_boards.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
-while read -r fname; do
-    file="$(basename "$fname" ".kicad_pcb")"
+exitcode=0
 
-    echo "Checking PCB: $file..."
-    #kicad-cli sch erc --severity-all --output "out/erc-$file" "$fname"
-    #kicad-cli pcb drc --severity-all --output "out/drc-$file" "$fname"
+while read -r fname; do
+    proj_path="${fname%.kicad_pro}"
+    proj_name="$(basename "$fname" ".kicad_pro")"
+
+    echo "::group::$proj_name"
 
     # Check if kicad project has a .kicad_dru file accompanying it
-    if [ ! -f "$(dirname "$fname")/$file.kicad_dru" ]; then 
+    if [ ! -f "$proj_path.kicad_dru" ]; then 
         echo "::error file=$fname::KiCAD Design Rules File Missing!"
+        exitcode=$((exitcode|1))
     fi
 
     # TODO report an error on library mismatch with symbol/footprint
     # TODO report an error if a common footprint is used (that has a bluesat replacement)
     # TODO report an error if a CubeMX footprint is used, should only use the symbol
     # TODO report an error if a sym-lib-table or fp-lib-table contains /home or C drive?
-    # TODO report an error if a board doesn't have a custom design rules file set
+
+    echo "Checking design files: $proj_name..."
+
+    kicad-cli sch erc --severity-all --exit-code-violations --output "out/erc-$proj_name" "$proj_path.kicad_sch"
+    if [ ! -f "$proj_path.kicad_dru" ]; then 
+        echo "::error file=$proj_name::ERC Error!"
+        exitcode=$((exitcode|2))
+    fi
+
+    kicad-cli pcb drc --severity-all --exit-code-violations --output "out/drc-$proj_name" "$proj_path.kicad_pcb"
+    if [ ! -f "$proj_path.kicad_dru" ]; then 
+        echo "::error file=$proj_name::DRC Error!"
+        exitcode=$((exitcode|4))
+    fi
 
 
+    echo "::endgroup::"
     echo ""
-done < <(find "$PWD" -type f -name "*.kicad_pcb")
+done < <(find "$PWD" -type f -name "*.kicad_pro")
+
+exit $exitcode
