f27a71b036d221c70647ef0096dc11e9fb41f461
bluesatunsw <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>
2026-03-20 00:56:41 +0000

    caninit.sh set txqueuelen=1000

--- utils/obc/caninit.sh
+++ utils/obc/caninit.sh
@@ -5,8 +5,10 @@ sudo modprobe can_raw
 
 sudo ip link set can-all down 
 sudo ip link set can-all type can bitrate 1000000 dbitrate 8000000 fd on loopback off
+sudo ip link set can-all txqueuelen 10000
 sudo ip link set can-all up
 
 sudo ip link set can-vesc down 
 sudo ip link set can-vesc type can bitrate 1000000 fd off loopback off
+sudo ip link set can-vesc txqueuelen 10000
 sudo ip link set can-vesc up
