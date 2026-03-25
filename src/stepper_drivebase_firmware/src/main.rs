b75c85897ee24e1afc5633c77d7ff0aa499f0b55
Jonah Hopkin <jonahhopkin@gmail.com>
2026-03-25 22:05:33 +1030

    Add encoders back into the mix

--- src/stepper_drivebase_firmware/src/main.rs
+++ src/stepper_drivebase_firmware/src/main.rs
@@ -189,13 +189,12 @@ fn main() -> ! {
         gpioa.pa10.into_input(),
         // Currently, the encoder does not work.
         // TODO: fix encoder, uncomment this when working
-        /*
         (
             gpiob.pb13.into_alternate(),
             gpiob.pb14.into_alternate(),
             gpiob.pb15.into_alternate(),
         ),
-        EncoderNcsPins(
+        encoder_bus::EncoderNcsPins(
             gpioc
                 .pc6
                 .into_push_pull_output_in_state(PinState::High)
@@ -214,7 +213,6 @@ fn main() -> ! {
                 .into(),
         ),
         dp.SPI2,
-        */
         dp.SPI3,
         &mut rcc,
     );
