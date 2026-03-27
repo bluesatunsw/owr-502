81a5f33ca7e2dbfcef4add80195a154553747c22
Aavin Fernando <aavin.fernando@gmail.com>
2026-03-27 21:26:36 +1030

    Update OBC utils for comp

--- utils/obc/wasd/wasd.sh
+++ utils/obc/wasd/wasd.sh
@@ -1,6 +1,6 @@
 function steer {
     echo "steering $1 $2 $3 $4"
-    yakut -i "CAN(can.media.socketcan.SocketCANMedia('can-all',64),44)" pub -N 1 \
+    yakut -i "CAN(can.media.socketcan.SocketCANMedia('can-all',64),44)" pub -N 3 \
         3000:reg.udral.physics.dynamics.rotation.Planar "{kinematics: {angular_position: $1 }}" \
         3010:reg.udral.physics.dynamics.rotation.Planar "{kinematics: {angular_position: $2 }}" \
         3020:reg.udral.physics.dynamics.rotation.Planar "{kinematics: {angular_position: $3 }}" \
@@ -8,7 +8,7 @@ function steer {
 }
 
 function drive {
-    yakut -i "CAN(can.media.socketcan.SocketCANMedia('can-vesc',8),43)" pub -N 1 \
+    yakut -i "CAN(can.media.socketcan.SocketCANMedia('can-vesc',8),43)" pub -N 3 \
         3050:reg.udral.service.actuator.common.sp.Scalar.0.1 "{value: $1}" \
         3060:reg.udral.service.actuator.common.sp.Scalar.0.1 "{value: $2}" \
         3070:reg.udral.service.actuator.common.sp.Scalar.0.1 "{value: $3}" \
@@ -19,10 +19,12 @@ function drive {
 NAME=${0,,}
 NAME=${NAME#./}
 
-VELOCITY="0.06"
+VELOCITY="0.03"
+ANG_VELOCITY="0.03"
 if [[ "$0" =~ [[:upper:]] ]] 
 then
-   VELOCITY="0.20"
+    VELOCITY="0.06"
+    ANG_VELOCITY="0.06"
 fi
 
 drive 0.0 0.0 0.0 0.0
@@ -47,6 +49,7 @@ case $NAME in
         ;;
 esac
 
+
 echo -n "Waiting for steering motion... "
 echo -n "2... "
 sleep 1
@@ -65,10 +68,10 @@ case $NAME in
         ;;
 
     a)
-        drive -$VELOCITY -$VELOCITY -$VELOCITY -$VELOCITY
+        drive -$ANG_VELOCITY -$ANG_VELOCITY -$ANG_VELOCITY -$ANG_VELOCITY
         ;;
       
     d)
-        drive $VELOCITY $VELOCITY $VELOCITY $VELOCITY
+        drive $ANG_VELOCITY $ANG_VELOCITY $ANG_VELOCITY $ANG_VELOCITY
         ;;
 esac
