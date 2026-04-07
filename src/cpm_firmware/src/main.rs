3557b394902881e93d4c710256c2a28c5a91b0c5
Evan La Fontaine <navelafontaine@gmail.com>
2026-04-07 18:57:39 +1000

    Firmware`1

--- src/cpm_firmware/src/main.rs
+++ src/cpm_firmware/src/main.rs
@@ -15,7 +15,7 @@ use cortex_m_rt::entry;
 use embedded_common::argb::{self, Colour};
 use fugit::RateExtU32;
 use stm32g4xx_hal::{
-    gpio::{GpioExt, Speed},
+    gpio::{GpioExt, PinState, Speed},
     prelude::*,
     pwr::{PwrExt, VoltageScale},
     rcc::*,
@@ -57,12 +57,34 @@ fn main() -> ! {
         pwr,
     );
 
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let gpiob = dp.GPIOB.split(&mut rcc);
     let gpioc = dp.GPIOC.split(&mut rcc);
-    let led_port_0 = gpioc.pc12.into_alternate().speed(Speed::VeryHigh);
-    let mut led_ctrl = argb::Controller::new(dp.UART5, led_port_0, 255, &mut rcc);
+
+    let _en24 = gpioc.pc6.into_push_pull_output_in_state(PinState::High);
+    let _en19 = gpioc.pc7.into_push_pull_output_in_state(PinState::High);
+    let _nen5a = gpioc.pc8.into_push_pull_output_in_state(PinState::Low);
+    let _nen5b = gpioc.pc9.into_push_pull_output_in_state(PinState::Low);
+
+    let lpi = gpioc.pc10.into_alternate().speed(Speed::Low);    
+    let lp0 = gpioc.pc12.into_alternate().speed(Speed::Low);
+    let lp1 = gpioa.pa2.into_alternate().speed(Speed::Low);
+    let lp2 = gpiob.pb9.into_alternate().speed(Speed::Low);
+    let lp3 = gpioc.pc4.into_alternate().speed(Speed::Low);
+
+    let mut lci = argb::Controller::new(dp.UART4, lpi, 255, &mut rcc);
+    let mut lc0 = argb::Controller::new(dp.UART5, lp0, 255, &mut rcc);
+    let mut lc1 = argb::Controller::new(dp.USART2, lp1, 255, &mut rcc);
+    let mut lc2 = argb::Controller::new(dp.USART3, lp2, 255, &mut rcc);
+    let mut lc3 = argb::Controller::new(dp.USART1, lp3, 255, &mut rcc);
+
     let leds = [Colour::AMBER; NUM_LEDS];
 
     loop {
-        led_ctrl.display(&leds);
+        lci.display(&[Colour::AMBER; 3]);
+        lc0.display(&leds);
+        lc1.display(&leds);
+        lc2.display(&leds);
+        lc3.display(&leds);
     }
 }
